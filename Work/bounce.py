e84d8551adea94cb22db61c0c5dcd803480dc6c2
mmy <1114519278@qq.com>
2023-08-30 16:05:29 +0800

    homework00

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,8 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100
+rate = 3/5
+for i in range(10):
+    print(round(height, 4))
+    height = height*rate
---
+++ Work/preliminaries.py
@@ -0,0 +1,5 @@
+# Exercise 1.26
+
+with open('Data/portfolio.csv', 'rt') as f:
+    data = f.read()
+print(data)
